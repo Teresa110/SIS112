b82dcdfb3e5e336ea149a532f181425fe608725c
Teresa110 <teresa.arteaga@ucb.edu.bo>
2024-08-16 10:43:17 -0400

    Create python.py
    programacion en python

---
+++ Python/python.py
@@ -0,0 +1,19 @@
+def HolaMundo():
+    print("hola mundo")
+
+def obteneredad():
+    obteneredad = input("Ingrese su edad:")
+    print("su edad es" , obteneredad)
+
+def calcularedad():
+    calculando = int(input("ingrese su año de nacimiento"))
+    edad= 2024-calculando
+    print("su edad es",edad)
+
+HolaMundo()
+obteneredad()
+calcularedad()
+
+
+
+
