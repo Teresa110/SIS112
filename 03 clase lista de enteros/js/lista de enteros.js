70166a83f368dc049a28517d429fb565367f6ee6
Teresa110 <teresa.arteaga@ucb.edu.bo>
2024-09-27 11:34:38 -0400

    lista de enteros

--- 03 clase lista de enteros/js/lista de enteros.js
+++ 03 clase lista de enteros/js/lista de enteros.js
@@ -6,6 +6,21 @@ class ListaEnteros {
     agregar(valor) {
         this.lista.push(valor);
     }
+
+    eliminar(valor) {
+        const index = this.lista.indexOf(valor);
+        if (index !== -1) {
+            this.lista.splice(index, 1);
+        }
+    }
+
+    buscar(valor) {
+        return this.lista.indexOf(valor);
+    }
+
+    ordenar() {
+        this.lista.sort((a, b) => a - b); // orden ascendente 
+    }
 }
 
 const miLista = new ListaEnteros();
@@ -21,6 +36,40 @@ document.getElementById('agregarBtn').addEventListener('click', () => {
     } else {
         alert('Por favor, ingresa un número válido.');
     }
+});
+
+document.getElementById('eliminarBtn').addEventListener('click', () => {
+    const valorInput = document.getElementById('valor');
+    const valor = Number(valorInput.value);
+    
+    if (!isNaN(valor)) {
+        miLista.eliminar(valor);
+        valorInput.value = '';
+        actualizarLista();
+    } else {
+        alert('Por favor, ingresa un número válido.');
+    }
+});
+
+document.getElementById('buscarBtn').addEventListener('click', () => {
+    const valorInput = document.getElementById('valor');
+    const valor = Number(valorInput.value);
+    
+    if (!isNaN(valor)) {
+        const index = miLista.buscar(valor);
+        if (index !== -1) {
+            alert(`El número ${valor} se encuentra en la lista en la posición ${index}.`);
+        } else {
+            alert(`El número ${valor} no se encuentra en la lista.`);
+        }
+    } else {
+        alert('Por favor, ingresa un número válido.');
+    }
+});
+
+document.getElementById('ordenarBtn').addEventListener('click', () => {
+    miLista.ordenar();
+    actualizarLista();
 });
 
 function actualizarLista() {
