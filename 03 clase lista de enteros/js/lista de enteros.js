8afb7d7499127b5c5819b5bce939a1b0cdea7454
Teresa110 <teresa.arteaga@ucb.edu.bo>
2024-10-04 10:13:06 -0400

    lista de enteros

--- 03 clase lista de enteros/js/lista de enteros.js
+++ 03 clase lista de enteros/js/lista de enteros.js
@@ -23,16 +23,87 @@ class ListaEnteros {
     }
 }
 
-let miLista = new ListaEnteros();
+const listaEnteros = new ListaEnteros();
+listaEnteros.agregar(21);
+listaEnteros.agregar(12);
+listaEnteros.agregar(15);
+listaEnteros.agregar(30);
+console.log(listaEnteros.lista);
+
+var a = listaEnteros.lista[0];
+console.log(a);
+
+var b = listaEnteros.lista[1];
+console.log(b);
+
+var c = listaEnteros.lista[2];
+console.log(c);
+
+var d = listaEnteros.lista[3];
+console.log(d);
+
+/*
+var datoMenor = -1;
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+var a = listaEnteros.lista[0];
+if(a > datoMenor){
+    datoMenor = a;
+}
+console.log("Dato Menor:" + datoMenor);
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+
+var b = listaEnteros.lista[1];
+if(a > b){
+    datoMenor = b;
+}
+console.log("Dato Menor:" + datoMenor);
+//--------------------------------------------------------------------------------------------------------------------------------------------------------
+var c = listaEnteros.lista[2];
+if(b > c){
+    datoMenor = c;
+}
+
+console.log("Dato Menor:" + datoMenor);
+
+var d = listaEnteros.lista[3];
+if(c > d){
+    datoMenor = d;
+}
+
+console.log("Dato Menor:" + datoMenor);
+
+*/
+
+var datoMenor = listaEnteros.lista[0]; 
+
+for (var i = 1; i < listaEnteros.lista.length; i++) {
+    var a = listaEnteros.lista[i];
+    if (a < datoMenor) { 
+        datoMenor = a; 
+    }
+    console.log("Dato Menor: " + datoMenor); 
+}
+
+var datoMayor = listaEnteros.lista[0];
+
+for (var i = 1; i < listaEnteros.lista.length; i++) {
+    var a = listaEnteros.lista[i];
+    if (a > datoMayor) { // Comparamos el elemento actual con datoMayor
+        datoMayor = a; // Actualizamos datoMayor si encontramos un mayor
+    }
+}
+
+console.log("Dato Mayor: " + datoMayor);
+
 
 function actualizarResultado() {
-    document.getElementById('result').innerHTML = `Resultado: [ ${miLista.lista.join(', ')} ]`;
+    document.getElementById('result').innerHTML = `Resultado: [ ${listaEnteros.lista.join(', ')} ]`;
 }
 
 function agregar() {
     const valor = parseInt(document.getElementById('inputValor').value);
     if (!isNaN(valor)) {
-        miLista.agregar(valor);
+        listaEnteros.agregar(valor);
         actualizarResultado();
         document.getElementById('inputValor').value = ''; // Limpiar input
     }
@@ -41,7 +112,7 @@ function agregar() {
 function eliminar() {
     const valor = parseInt(document.getElementById('inputValor').value);
     if (!isNaN(valor)) {
-        miLista.eliminar(valor);
+        listaEnteros.eliminar(valor);
         actualizarResultado();
         document.getElementById('inputValor').value = ''; // Limpiar input
     }
@@ -50,12 +121,12 @@ function eliminar() {
 function buscar() {
     const valor = parseInt(document.getElementById('inputValor').value);
     if (!isNaN(valor)) {
-        const index = miLista.buscar(valor);
+        const index = listaEnteros.buscar(valor);
         alert(index !== -1 ? `El número ${valor} está en la posición ${index}` : `${valor} no se encuentra en la lista.`);
     }
 }
 
 function ordenar() {
-    miLista.ordenar();
+    listaEnteros.ordenar();
     actualizarResultado();
 }
